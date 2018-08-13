48df69d6c10d322e0b029dadbefc378161b7057f
korman <korman.yuri@gmail.com>
2018-08-13 14:56:30 +0300

    add more info in response

--- index.php
+++ index.php
@@ -3,18 +3,20 @@ require_once 'vendor/autoload.php';
 
 use Twilio\Rest\Client;
 
-$data = json_decode(file_get_contents('php://input'), true);
+if ($_SERVER['REQUEST_METHOD'] != 'OPTIONS') {
+    $data = json_decode(file_get_contents('php://input'), true);
 
-$host = 'jingjing-mqtt.fenglinfl.com';
-$port = '1883';
-$user = 'jingjing';
-$pass = 'eisen9999jingjing';
+    $host = 'jingjing-mqtt.fenglinfl.com';
+    $port = '1883';
+    $user = 'jingjing';
+    $pass = 'eisen9999jingjing';
 
-$minutes = strlen($data['minutes']) == 1 ? $data['minutes'] . '.' : $data['minutes'];
+    $minutes = strlen($data['minutes']) == 1 ? $data['minutes'] . '.' : $data['minutes'];
 
-$client = new \Mosquitto\Client();
-$client->setCredentials($user, $pass);
-$client->connect($host, $port, 5);
-$int = $client->publish($data['imei'], $minutes . "m", 0, 0);
+    $client = new \Mosquitto\Client();
+    $client->setCredentials($user, $pass);
+    $client->connect($host, $port, 5);
+    $int = $client->publish($data['imei'], $minutes . "m", 0, 0);
 
-print($data['imei'] . ':' . $minutes . "m " . $int);
+    print($data['imei'] . ':' . $minutes . "m " . $int);
+}
