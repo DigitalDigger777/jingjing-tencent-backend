be72e6f268396c77c0fc681e1dd8f713bea7cab0
korman <korman.yuri@gmail.com>
2018-08-13 14:35:31 +0300

    add more info in response

--- index.php
+++ index.php
@@ -6,15 +6,15 @@ use Twilio\Rest\Client;
 $data = json_decode(file_get_contents('php://input'), true);
 
 $host = 'jingjing-mqtt.fenglinfl.com';
-$port = 1883;
+$port = '1883';
 $user = 'jingjing';
 $pass = 'eisen9999jingjing';
 
-$minutes = strlen($data['minutes']) == 1 ? strlen($data['minutes']) . '.' : strlen($data['minutes']);
+$minutes = strlen($data['minutes']) == 1 ? $data['minutes'] . '.' : $data['minutes'];
 
 $client = new \Mosquitto\Client();
 $client->setCredentials($user, $pass);
 $client->connect($host, $port, 5);
-$client->publish($data['imei'], $minutes . "m", 0, 0);
+$int = $client->publish($data['imei'], $minutes . "m", 0, 0);
 
-print($data['imei'] . ':' . $minutes . "m");
+print($data['imei'] . ':' . $minutes . "m " . $int);
